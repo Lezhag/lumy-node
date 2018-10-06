b5876094c34f83b8de12786d7989ea91c05ac0ab
Oleg Glybchenko <oleg.glyb@gmail.com>
2018-10-06 22:06:49 +0300

    clean up and update

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
  * Created by Oleg on 26/12/2015.
  */
 var express = require('express');
-var nodeMailer = require('nodemailer');
 
 var app = express();
 
-var smtpTransport = nodeMailer.createTransport("SMTP",{
-    service: "Gmail",
-    auth: {
-        user: "lumy.mailer@gmail.com",
-        pass: "n1NZgxUqJbQE"
-    }
-});
+// var smtpTransport = nodeMailer.createTransport("SMTP",{
+//     service: "Gmail",
+//     auth: {
+//         user: "lumy.mailer@gmail.com",
+//         pass: "n1NZgxUqJbQE"
+//     }
+// });
+
 app.set('port', (process.env.PORT || 8080));
 
 app.use(express.static(__dirname + '/public'));
@@ -23,30 +23,6 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
     response.render('public/index');
-});
-
-app.get('/send', function (request, response) {
-    var mailOptions = {
-        to: "lumyflowers@gmail.com",
-        subject: request.fullName,
-        text: " שם" + request.fullName + "/n" +
-        " טלפון" + request.phone + "/n" +
-        " מייל" + request.email + "/n" +
-        " תאריך" + request.date + "/n" +
-        " שעה" + request.hour + "/n" +
-        " מקום האירוע" + request.place + "/n" +
-        " תיאור האירוע" + request.description + "/n"
-    };
-    console.log(mailOptions);
-    smtpTransport.sendMail(mailOptions, function (error, mailResponse) {
-        if (error) {
-            //console.log(error);
-            response.end("error: " + error);
-        } else {
-            //console.log("Message sent: " + mailResponse.message);
-            response.end("mail is sent");
-        }
-    })
 });
 
 app.listen(app.get('port'), function() {
